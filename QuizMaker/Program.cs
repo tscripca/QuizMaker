88cdec588d004f303d29cc1ea0c97f67d8194d55
tscripca <scripcatudor1@gmail.com>
2024-02-02 20:35:24 +0000

    Invalid Cast Exception error when serializing

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.Xml;
 using System.Xml.Serialization;
 using QuizMaker;
 
-namespace QuizMaker 
+namespace QuizMaker
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            QnA.
-        }
-    }   
+            QnA myQuestions = new QnA();
+
+            Console.WriteLine("Type question: ");
+            myQuestions.userQuestion = Console.ReadLine();
+            Console.WriteLine("Give answer: ");
+            myQuestions.userAnswer = Console.ReadLine();      
+            
+            XmlSerializer serializer = new XmlSerializer(typeof(List<QnA>));
+
+            var path = @"C:\Users\scrip\Desktop\.xml";
+            using (FileStream file = File.Create(path))
+            {
+                serializer.Serialize(file, myQuestions);
+                Console.WriteLine();
+            }
+        }         
+    }  
+    
+    public class QnA
+    {
+        public string userQuestion { get; set; }
+        public string userAnswer { get; set; }       
+    }
 }
