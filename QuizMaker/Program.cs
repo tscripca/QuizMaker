a577d5fb03e70981f093a6f00f463f123c5f8e29
tscripca <scripcatudor1@gmail.com>
2024-04-28 09:38:06 +0100

    fill in a new card every time with Q and A

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -6,16 +6,11 @@ namespace QuizMaker
     {
         static void Main(string[] args)
         {
-            QnACard myCard1 = new QnACard();
-
-            myCard1.gameQuestion = UIMethods.GetUserQuestion();
-            myCard1.answersList = Logic.GetUserAnswers();
-
-            Console.WriteLine($"Your question was: {myCard1.gameQuestion}");
-
-            foreach (string answer in myCard1.answersList)
+            for(int userQuestion = 0; userQuestion <= 2; userQuestion++)
             {
-                Console.WriteLine($"The list of answers contains: {answer}");
+                QnACard myCard1 = new QnACard();
+                myCard1.gameQuestion = UIMethods.GetUserQuestion();
+                myCard1.answersList = Logic.GetUserAnswers();
             }
         }        
     }
